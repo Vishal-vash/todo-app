6e2523921bc249bf45a51a076dfd61e7d9f0618b
Vishal Vasishat <vishal.vash@gmail.com>
2017-06-13 00:34:17 +0530

    Create todo.service.ts

--- client/src/app/todo/todo.service.ts
+++ client/src/app/todo/todo.service.ts
@@ -1,33 +1,39 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/Core';
 import { Http, Headers } from '@angular/http';
-import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/map';
 
 @Injectable()
-
 export class TodoService{
-    constructor(private http: Http){}
-
-    addTask(task){
-        var headers = new Headers();
-        headers.append('content-type','application/json');
-        return this.http.post('api/tasklist', JSON.stringify(task), {headers : headers})
-                .map(response => response.json());
+    constructor(private http : Http){
+        //Do something
     }
 
+    //Get A Task
     getTasks(){
-        return this.http.get('api/tasklist')
+        return this.http.get('api/todo')
                     .map(response => response.json());
     }
 
-    removeTask(id){
-        return this.http.delete('api/tasklist/'+id)
+
+    //Add A Task
+    addTask(task){
+        var headers = new Headers();
+        headers.append('content-type', 'application/json');
+        return this.http.post('api/todo', JSON.stringify(task), {headers : headers})
                     .map(response => response.json())
     }
 
+    //Delete A Task
+    removeTask(id){
+        return this.http.delete('api/todo/'+id)
+                    .map(response => response.json())
+    }
+
+    //Complete Task
     completeTask(task){
         var headers = new Headers();
         headers.append('content-type', 'application/json');
-        return this.http.put('api/tasklist/'+ task._id, JSON.stringify(task), {headers : headers})
-                    .map(response => response.json())
+        return this.http.put('api/todo/'+task._id, JSON.stringify(task), {headers : headers})
+                        .map(response => response.json())
     }
 }
