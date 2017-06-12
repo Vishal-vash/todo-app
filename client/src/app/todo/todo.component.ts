414ff6831d3de3cf11e2be2bbef4c2d31674c156
Vishal Vasishat <vishal.vash@gmail.com>
2017-06-13 00:33:53 +0530

    Create todo.component.ts

--- client/src/app/todo/todo.component.ts
+++ client/src/app/todo/todo.component.ts
@@ -1,55 +1,61 @@
 import { Component, OnInit } from '@angular/core';
-
 import { TodoService } from './todo.service';
 
 @Component({
-    selector : "todolist",
-    templateUrl : "./todo.component.html",
-    styleUrls : ["./todo.component.css"],
+    selector : 'todo-list',
+    templateUrl : './todo.component.html',
+    styleUrls : ['./todo.component.css'],
     providers : [TodoService]
 })
 
 export class TodoComponent implements OnInit{
+    //Variables
     tasks : Array<Object> = [];
-    showAddTaskForm : Boolean = false;
-    task = {
-        completed : false,
-        name : ''
+    task : Object = {
+        taskName : '',
+        isCompleted : false
     }
+    tasksDone : number = 0;
+    showAddTask : boolean = false;
+    innerHeight : number = 0;
+    inputFocus : boolean = false;
     constructor(private todoService : TodoService){
         //do something
+        this.innerHeight = window.outerHeight - 125;
     }
 
     getTasks(){
-        this.todoService.getTasks().subscribe(data => this.tasks = data);
+        this.todoService.getTasks().subscribe(tasks => (this.tasks = tasks, this.tasksDone = 0, tasks.map((v,i) => v['isCompleted'] ? this.tasksDone += 1 : this.tasksDone)));
     }
 
     addTask(){
-        this.todoService.addTask(this.task).subscribe(data => this.tasks.push(data));
+        this.todoService.addTask(this.task).subscribe(task => this.tasks.push(task));
         this.getTasks();
-        this.showAddTaskForm = !this.showAddTaskForm;
-        this.task = null;
+        this.task = {
+            taskName : '',
+            isCompleted : false
+        }
+        this.showAddTask = false;
+        this.inputFocus = false;
     }
 
     removeTask(id){
-        this.todoService.removeTask(id)
-                .subscribe(data => this.tasks.filter((v,i) => v["_id"] != id));
+        this.todoService.removeTask(id).subscribe(task => this.tasks.filter((v,i) => v["_id"] != id));
         this.getTasks();
     }
 
     completeTask(task){
         var _task = {
             _id : task._id,
-            name : task.name,
-            completed : !task.completed
+            taskName : task.taskName,
+            isCompleted : !task.isCompleted
         }
-        this.todoService.completeTask(_task)
-                .subscribe(data => console.log("Done Sucessfully"));
+       // this.todoService.completeTask(_task).subscribe(data => (data.isCompleted) ? (this.tasksDone -= 1) : (this.tasksDone += 1));
+        this.todoService.completeTask(_task).subscribe(data => console.log("Working"));
         this.getTasks();
     }
 
     ngOnInit(){
         this.getTasks();
     }
-
 }
