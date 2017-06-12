00cd4d21a762d911d2de73ce786aae3896cc31f9
Vishal Vasishat <vishal.vash@gmail.com>
2017-06-13 00:35:58 +0530

    Create server.js

--- server.js
+++ server.js
@@ -1,42 +1,41 @@
-//Get Dependencies
+//Get dependenecies
+const express = require('express'),
+      bodyParser = require('body-parser'),
+      path = require('path'),
+      mongoose = require('mongoose'),
+      http = require('http');
 
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const path = require('path');
-const http = require('http');
+//Connect TO DB 
+mongoose.connect('mongodb://localhost/todo')
 
-//Connect to DB
-mongoose.connect('mongodb://localhost/tasklist')
-
-//Get Api Routes
+//Get Our API Routes
 const api = require('./server/routes/api');
 
 const app = express();
 
-//Parses for POST data
+//Parsing POST Data
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({
+    extended : false
+}))
 
-//Point static path to dist
-app.use(express.static(path.join(__dirname, '/client/dist')))
+//Point path to get all static files like HTML/CSS/Images/JS
+app.use(express.static(path.join(__dirname + '/client/dist')));
 
-//Set Api Routes
-app.use('/api', api);
+//Set Our API routes
+app.use('/api', api)
 
-//Catch all other routes and return Index file
+//Get all routes and render index html file
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, "/client/dist/index.html"));
+    res.sendFile(path.join(__dirname+'/client/dist/index.html'))
 })
 
-//Get Port from Enviroment and store in express
+//Store the port in express
 const Port = process.env.Port || 3000;
 app.set('port', Port);
 
-//Create HTTP Server
+//Create http server
 const server = http.createServer(app);
 
-//Listen to the Port on all Network interfaces
-server.listen(Port, () => {
-    console.log("Server is running on : " + Port)
-})
+//Listen to the server at provided Port
+server.listen(Port, () => console.log("Server started at " + Port)); 
