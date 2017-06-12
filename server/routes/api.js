de17372563da3fbdcf01f608c4d6ff66bc25cb31
Vishal Vasishat <vishal.vash@gmail.com>
2017-06-13 00:37:46 +0530

    Create api.js

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,41 +1,43 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express'),
+      router = express.Router();
 
 //Get Model
-const tasklist = require('../models/tasklist');
+const todo = require('../models/todo.model')
 
-//Get Api Listing
-router.get('/tasklist', (req, res) => {
-    tasklist.find({}, (err, resource) =>
-        (err) ? res.send(err).status(404) : res.send(resource).status(200)
-    )
-})
-
-//Add Task
-router.post('/tasklist', (req, res) => {
-    var Tasklist = new tasklist(req.body);
-    Tasklist.save((err, resource) => 
-        (err) ? res.send(err).status(501) : res.send(resource).status(201)
-    )
-})
-
-router.delete('/tasklist/:id', (req,res) => {
-    var id = req.params.id;
-    tasklist.remove({_id : id}, (err, resource) => {
-        (err) ? res.send(err).status(404) : res.send(resource).status(200)
+//Get API
+router.get('/todo', (req, res) => {
+    todo.find({}, (err, resource) => {
+        err ? res.send(err).status(404) : res.send(resource).status(200)
     })
 })
 
-router.put('/tasklist/:id', (req, res) => {
+//Post Task
+router.post('/todo', (req, res) => {
+    var Todo = new todo(req.body);
+    Todo.save((err, resource) => {
+        err ? res.send(err).status(500) : res.send(resource).status(200)
+    })
+})
+
+//Delete Task
+router.delete('/todo/:id', (req, res) => {
     var id = req.params.id;
-    tasklist.findByIdAndUpdate(
-       id,
-       {
-            name : req.body.name,
-            completed : req.body.completed
+    todo.remove({_id : id}, (err, resource) => {
+        err ? res.send(err).status(500) : res.send(resource).status(200)
+    })
+})
+
+//Complete Task
+router.put('/todo/:id', (req, res) => {
+    var id = req.params.id;
+    todo.findByIdAndUpdate(
+        id,
+        {
+            isCompleted : req.body.isCompleted
         },
         (err, resource) => {
-        (err) ? res.send(err).status(500) : res.send(resource).status(200)
-    })
+            err ? res.send(err).status(501) : res.send(resource).status(201)
+        }
+    )
 })
 module.exports = router;
